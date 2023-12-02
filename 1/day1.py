caa199e9353bc3ed643796dc587ad4bf5a3415d9
Lucas Kalani Ross <lkross@Lucass-MacBook-Pro.local>
2023-12-01 19:57:13 -0800

    changed method for day 1 method 2. instead of changing all words to numbers, only change the first and last word and then find first and last digits

--- 1/day1.py
+++ 1/day1.py
@@ -1,32 +1,55 @@
 f = open("1/input.txt", "r").readlines()
 rtn = 0
 
-for line in f: #go through each line
+for l in f: #go through each line
+    line = l.removesuffix("\n")
+    print(line)
     #convert word numbers to line
     words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    first_occur = len(line) - 1
+    first_word = ""
+    last_occur = 0
+    last_word = ""
+
     for word in words:
-        index = line.find(word)
-        if index != -1:
-            #add the first letter of the word, the number, and the last letter of the word
-            line = line[0:index+1] + str(words.index(word) + 1) + line[index+len(word)-1:]
+        i = line.find(word)
+        if i < first_occur and i != -1:
+            first_word = word
+            first_occur = i
+
+        j = line.rfind(word)
+        if j > last_occur and i != -1:
+            last_word = word
+            last_occur = j
+        
+    print(first_word, first_occur, last_word, last_occur)
+
+    line = [x for x in line]
+    if last_word != "":
+        line[last_occur] = str(words.index(last_word) + 1)
+    if first_word != "":
+        line[first_occur] = str(words.index(first_word) + 1)
+
+    line = "".join(line)
+
+    print(line)
 
     #find first and last numbers in string
     start = 0
     end = len(line) - 1
 
-    a = 0 #first number
-    b = 0 #second number
+    digits = [] #first and last number
 
     while not(str(line[start]).isnumeric()):
         start += 1
 
-    a = str(line[start])
+    digits += str(line[start])
     
     while not(str(line[end]).isnumeric()):
         end -= 1
 
-    b = str(line[end])
+    digits += str(line[end])
     
-    rtn += int(a + b)    
+    rtn += int("".join(digits))
 
 print(rtn)
