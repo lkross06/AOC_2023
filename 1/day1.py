cec82dfe6e5f269e4fb2e70599a068e88a832437
Lucas Kalani Ross <lkross@Lucass-MacBook-Pro.local>
2023-12-01 18:14:58 -0800

    added pretty comments

--- 1/day1.py
+++ 1/day1.py
@@ -1,14 +1,16 @@
 f = open("1/input.txt", "r").readlines()
-
 rtn = 0
+
 for line in f: #go through each line
     #convert word numbers to line
     words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     for word in words:
         index = line.find(word)
         if index != -1:
+            #add the first letter of the word, the number, and the last letter of the word
             line = line[0:index+1] + str(words.index(word) + 1) + line[index+len(word)-1:]
 
+    #find first and last numbers in string
     start = 0
     end = len(line) - 1
 
