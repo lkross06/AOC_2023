70a8bccf4fea41bf71e360a9edaa3196b047ea4f
Lucas Kalani Ross <lkross@Lucass-MacBook-Pro.local>
2023-12-01 18:13:30 -0800

    initial commit. added day 1 code. why it no work for part 2 :(

---
+++ 1/day1.py
@@ -0,0 +1,30 @@
+f = open("1/input.txt", "r").readlines()
+
+rtn = 0
+for line in f: #go through each line
+    #convert word numbers to line
+    words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for word in words:
+        index = line.find(word)
+        if index != -1:
+            line = line[0:index+1] + str(words.index(word) + 1) + line[index+len(word)-1:]
+
+    start = 0
+    end = len(line) - 1
+
+    a = 0 #first number
+    b = 0 #second number
+
+    while not(str(line[start]).isnumeric()):
+        start += 1
+
+    a = str(line[start])
+    
+    while not(str(line[end]).isnumeric()):
+        end -= 1
+
+    b = str(line[end])
+    
+    rtn += int(a + b)    
+
+print(rtn)
