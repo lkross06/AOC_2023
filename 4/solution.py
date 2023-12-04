8afe2c67850cae8942e6216a88a3a93f7a1af092
Lucas Kalani Ross <lkross@Lucass-MacBook-Pro.local>
2023-12-03 21:08:19 -0800

    part 1 done

--- 4/solution.py
+++ 4/solution.py
@@ -1,1 +1,37 @@
-#solution haha
+f = open("4/input.txt", "r")
+
+rtn = 0
+
+for l in range(len(f)):
+    line = f[l].removesuffix("\n").replace(": ", "|")
+
+    line = line.split("|")[1:]
+
+    print(line)
+
+    win = [x for x in line[0].split(" ")]
+
+    for i in win:
+        if i == "":
+            win.remove(i)
+
+    given = [x for x in line[1].split(" ")]
+
+    for i in given:
+        if i == "":
+            given.remove(i)
+
+    total = 0
+
+    for i in given:
+        if i in win:
+            if total == 0:
+                total = 1
+            else:
+                total *= 2
+    
+    print(total)
+
+    rtn += total
+
+print(rtn)
