53690b1191cf1899adcdf0ec4349b652bc774c54
Lucas Kalani Ross <lkross@Lucass-MacBook-Pro.local>
2023-12-05 22:23:07 -0800

    part 1 done so ez

---
+++ 6/solution.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+import re
+
+start = datetime.now()
+
+f = [x.removesuffix("\n") for x in list(open("6/input.txt", "r"))]
+
+t = [int(x) for x in re.findall("\d+", f[0])]
+d = [int(x) for x in re.findall("\d+", f[1])]
+
+def get_num_winners(total_time, threshold_distance): #i think the d(t) function is concave + quadratic
+    i = 1
+    j = total_time
+
+    d1 = 0
+    d2 = 0
+
+    while i <= j and d1 <= threshold_distance and d2 <= threshold_distance:
+        d1 = (total_time - i) * i
+        d2 = (total_time - j) * j
+
+        if d1 <= threshold_distance:
+            i += 1
+
+        if d2 <= threshold_distance:
+            j -= 1
+
+    return j - i + 1
+
+product = 1
+
+for i in range(len(t)):
+    product *= get_num_winners(t[i], d[i])
+
+print(product)
+print(datetime.now()-start)
+
+    
